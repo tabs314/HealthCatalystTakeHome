bb2cf0d5cd60686488505d5a91bba9be3e8693c7
tabs314 <michael.tabet@gmail.com>
2017-03-07 14:42:36 -0700

    Searching working (mostly) - now add image support

--- HealthCatalystTest/Controllers/HomeController.cs
+++ HealthCatalystTest/Controllers/HomeController.cs
@@ -23,27 +23,38 @@ namespace HealthCatalystTest.Controllers
         [HttpGet]
         public JsonResult Search()
         {
-            string firstName = this.Request.QueryString.Get("first_name_search");
-            string lastName = this.Request.QueryString.Get("last_name_search");
+            string[] searchCriteria = this.Request.QueryString.Get("search_criteria").Split(' ');
             bool strictSearch = this.Request.QueryString.Get("strict_match") != null;
             int pageSize = 20;
             int pageNumber = 0;
 
             try
             {
-                pageSize = int.Parse(this.Request.QueryString.Get("page_size"));
-                pageNumber = int.Parse(this.Request.QueryString.Get("page_number"));
+                pageSize = Math.Max(int.Parse(this.Request.QueryString.Get("page_size")), 0); //Always positive page size
+                pageNumber = Math.Max(int.Parse(this.Request.QueryString.Get("page_number")), 0); //Always positive page number
             }
-            catch (Exception e) //Catch invalid numbers here
+            catch (Exception e) //Catch invalid numbers here - defaults should hold so continue
             {
 
             }
 
-            if (!strictSearch)
+            string firstName = "", lastName = "";
+            List<string> normalizedSearchCriteria = new List<string>();
+            if (strictSearch)
             {
-                //Normalize strings (in english) for cast
-                firstName = firstName.ToLower();
-                lastName = lastName.ToLower();
+                firstName = searchCriteria[0].Trim();
+                if (searchCriteria.Length >= 2)
+                {
+                    lastName = searchCriteria[1].Trim();
+                }
+            }
+            else
+            {
+                //We'll normalize to lower case
+                searchCriteria.ToList().ForEach(x => {
+                    if (!String.IsNullOrWhiteSpace(x))
+                        normalizedSearchCriteria.Add(x.ToLower());
+                    });
             }
             List<UserInformationModel> userList = new List<UserInformationModel>();
 
@@ -51,21 +62,40 @@ namespace HealthCatalystTest.Controllers
             {
                 if (strictSearch)
                 {
-                    var users = (from u in db.UserInformation
-                                 where u.FirstName.Equals(firstName) && u.LastName.Equals(lastName)
-                                 orderby u.LastName
-                                 select u).Skip(pageSize * pageNumber).Take(pageSize);
+                    if (!String.IsNullOrWhiteSpace(lastName))
+                    {
+                        var users = (from u in db.UserInformation
+                                     where u.FirstName.Equals(firstName) && u.LastName.Equals(lastName)
+                                     orderby u.LastName
+                                     select u).Skip(pageSize * pageNumber).Take(pageSize);
 
-                    userList.AddRange(users.ToList());
+                        userList.AddRange(users.ToList());
+                    }
+                    else
+                    {
+                        //In this case, consider "first name" across first and last name
+                        var users = (from u in db.UserInformation
+                                     where u.FirstName.Equals(firstName) || u.LastName.Equals(firstName)
+                                     orderby u.LastName
+                                     select u).Skip(pageSize * pageNumber).Take(pageSize);
+
+                        userList.AddRange(users.ToList());
+                    }
                 }
                 else
                 {
-                    var users = (from u in db.UserInformation
-                                where u.FirstName.ToLower().Contains(firstName) && u.LastName.ToLower().Contains(lastName)
-                                orderby u.LastName
-                                select u).Skip(pageSize * pageNumber).Take(pageSize);
+                    Dictionary<int, UserInformationModel> uniqueUsers = new Dictionary<int, UserInformationModel>();
 
-                    userList.AddRange(users.ToList());
+                    foreach(string searchTerm in normalizedSearchCriteria)
+                    {
+                       
+                        var users = (from u in db.UserInformation
+                                     where u.FirstName.ToLower().Contains(searchTerm) || u.LastName.ToLower().Contains(searchTerm)
+                                     select u);
+                        users.ToList().ForEach(u => uniqueUsers[u.id] = u);
+                    }
+
+                    userList = uniqueUsers.Values.OrderBy(u => u.LastName).Skip(pageNumber * pageSize).Take(pageSize).ToList();
                 }
                 
             }
